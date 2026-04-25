74afa71fe68919a73a481e80ca189170b819a822
Pedrocf01 <pedrocf01@gmail.com>
2026-04-25 14:18:47 -0300

    Fix URL validation for blocking websites

--- src/main/java/com/pedrocf01/url_shortener/domain/services/UrlExistenceValidator.java
+++ src/main/java/com/pedrocf01/url_shortener/domain/services/UrlExistenceValidator.java
@@ -15,11 +15,23 @@ public class UrlExistenceValidator {
             log.debug("Checking if URL exists: {}", urlString);
             URL url = new URI(urlString).toURL();
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            String browsers = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+            connection.setRequestProperty("User-Agent", browsers);
+
             connection.setRequestMethod("HEAD");
             connection.setConnectTimeout(5000); // 5 seconds
             connection.setReadTimeout(5000);
 
             int responseCode = connection.getResponseCode();
+
+            if (responseCode == 403 || responseCode == 405) {
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestProperty("User-Agent", browsers);
+                connection.setRequestMethod("GET");
+                responseCode = connection.getResponseCode();
+            }
+
             return (responseCode >= 200 && responseCode < 400); // 2xx and 3xx are valid
         } catch (Exception e) {
             log.error("Error while checking URL: {}", urlString, e);
