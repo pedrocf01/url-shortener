71a2423cb676ee6f0e7e3f7ac48e3aab9d04b430
Pedrocf01 <pedrocf01@gmail.com>
2026-05-22 23:30:26 -0300

    Containerize the application with Docker

--- src/main/java/com/pedrocf01/url_shortener/domain/services/ShortUrlService.java
+++ src/main/java/com/pedrocf01/url_shortener/domain/services/ShortUrlService.java
@@ -111,8 +111,8 @@ public class ShortUrlService {
         if(shortUrl.getExpiresAt() != null && shortUrl.getExpiresAt().isBefore(Instant.now()))
             return Optional.empty();
 
-        if(shortUrl.getIsPrivate() != null && shortUrl.getCreatedBy() != null
-                && !Objects.equals(shortUrl.getCreatedBy().getId(), userId))
+        if(shortUrl.getIsPrivate() != null && shortUrl.getIsPrivate() &&
+           shortUrl.getCreatedBy() != null  && !Objects.equals(shortUrl.getCreatedBy().getId(), userId))
             return Optional.empty();
 
         shortUrl.setClickCount(shortUrl.getClickCount() + 1);
